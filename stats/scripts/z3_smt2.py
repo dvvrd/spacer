2b566e7e4f87067b2514401c3f62da773154316d
Anvesh Komuravelli <anvesh@cs.cmu.edu>
2015-01-23 18:27:47 -0500

    adjusting and adding options in script

--- stats/scripts/z3_smt2.py
+++ stats/scripts/z3_smt2.py
@@ -32,10 +32,6 @@ def parseArgs (argv):
     p.add_argument ('--validate-theory-core', dest='validate_theory_core',
                     help='validate every theory core',
                     action='store_true', default=False)
-    p.add_argument ('--from-lvl', dest='from_lvl',
-                    type=int,
-                    help='start level for query predicate',
-                    action='store', default=0)
     p.add_argument ('--print-stats', dest='print_stats',
                     help='flag to print stats',
                     action='store_true', default=False)
@@ -59,6 +55,15 @@ def parseArgs (argv):
     p.add_argument ('--flexible-trace', dest='flexible_trace',
                     help='enable generation of long cexes',
                     action='store_true', default=False)
+    p.add_argument ('--skip-propagate', dest='skip_propagate',
+                    help='skip propagation phase, emulating BMC',
+                    action='store_true', default=False)
+    p.add_argument ('--keep-obligations', dest='keep_obligations',
+                    help='keep obligations across levels',
+                    action='store_true', default=False)
+    p.add_argument ('--max-lvl', dest='max_lvl',
+                    help='max query level', type=int,
+                    action='store', default=-1)
 
     return p.parse_args (argv)
 
@@ -93,26 +98,26 @@ def main (argv):
 
     if not args.slice:
         print 'No slicing'
-        z3_args += ' fixedpoint.slice=false'
+        z3_args += ' fixedpoint.xform.slice=false'
 
     if not args.inline:
         print 'No inlining'
-        z3_args += ' fixedpoint.inline_linear=false'
-        z3_args += ' fixedpoint.inline_eager=false'
+        z3_args += ' fixedpoint.xform.inline_linear=false'
+        z3_args += ' fixedpoint.xform.inline_eager=false'
 
     print 'Engine: ', args.engine
 
     if (args.validate):
-        z3_args += ' fixedpoint.validate_result=true'
+        z3_args += ' fixedpoint.pdr.validate_result=true'
 
     if (args.answer):
-        z3_args += ' fixedpoint.print_answer=true'
+        z3_args += ' fixedpoint.print.answer=true'
 
     z3_args += ' fixedpoint.engine='
     z3_args += args.engine
 
     if args.no_utvpi:
-        z3_args += ' fixedpoint.use_utvpi=false'
+        z3_args += ' fixedpoint.pdr.utvpi=false'
 
     if args.lazy_reach_check:
         z3_args += ' fixedpoint.eager_reach_check=false'
@@ -121,25 +126,34 @@ def main (argv):
         z3_args += ' fixedpoint.validate_theory_core=true'
 
     if args.print_stats:
-        z3_args += ' fixedpoint.print_statistics=true'
+        z3_args += ' fixedpoint.print.statistics=true'
 
     if args.dfs:
-        z3_args += ' fixedpoint.bfs_model_search=false'
+        z3_args += ' fixedpoint.pdr.bfs_model_search=false'
 
     if int(args.order_children)==1:
         z3_args += ' fixedpoint.order_children=1'
 
     if args.array_blast:
-        z3_args += ' fixedpoint.array_blast=true'
+        z3_args += ' fixedpoint.xform.array_blast=true'
 
     if args.array_blast_full:
-        z3_args += ' fixedpoint.array_blast_full=true'
+        z3_args += ' fixedpoint.xform.array_blast_full=true'
 
     if args.use_heavy_mev:
         z3_args += ' fixedpoint.use_heavy_mev=true'
 
     if args.flexible_trace:
-        z3_args += ' fixedpoint.flexible_trace=true'
+        z3_args += ' fixedpoint.pdr.flexible_trace=true'
+
+    if args.skip_propagate:
+        z3_args += ' fixedpoint.pdr.skip_propagate=true'
+
+    if args.keep_obligations:
+        z3_args += ' fixedpoint.reset_obligation_queue=false'
+
+    if int(args.max_lvl) >= 0:
+        z3_args += ' fixedpoint.pdr.max_level={}'.format (args.max_lvl)
 
     z3_args += ' ' + args.file
 
