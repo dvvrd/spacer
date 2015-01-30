97d5a6a563aa6fb0e6ad4d5e20883383d040f413
Arie Gurfinkel <arie@cmu.edu>
2015-01-30 17:14:31 -0500

    reach_fact always has a rule
    set rule in the constructor.
    convert pointers to references.

--- src/muz/spacer/spacer_context.cpp
+++ src/muz/spacer/spacer_context.cpp
@@ -785,9 +785,8 @@ namespace spacer {
         const datalog::rule* r = it->m_key;
         if (r->get_uninterpreted_tail_size () == 0)
         {
-          fact = alloc (reach_fact, m, m_rule2transition.find (r),
+          fact = alloc (reach_fact, m, *r, m_rule2transition.find (r),
                         get_aux_vars (*r), true);
-          fact->set_rule (*r);
           add_reach_fact (*fact);
         }
       }
@@ -2052,7 +2051,7 @@ namespace spacer {
 
         // get and discard query rule
         fact = m_query->get_last_reach_fact ();
-        r = fact->get_rule ();
+        r = &fact->get_rule ();
 
         unsigned cex_depth = 0;
 
@@ -2088,7 +2087,7 @@ namespace spacer {
             }
             fact = facts.get (curr - cex_depth); // discount the number of markers
             // get rule justifying the derivation of fact at pt
-            r = fact->get_rule ();
+            r = &fact->get_rule ();
             TRACE ("spacer",
                     tout << "next rule: " << r->name ().str () << "\n";
                   );
@@ -2127,7 +2126,7 @@ namespace spacer {
 
         // get and discard query rule
         fact = m_query->get_last_reach_fact ();
-        r = fact->get_rule ();
+        r = &fact->get_rule ();
 
         // initialize queues
         // assume that the query is only on a single predicate
@@ -2152,7 +2151,7 @@ namespace spacer {
             pt = pts.get (curr);
             fact = facts.get (curr);
             // get rule justifying the derivation of fact at pt
-            r = fact->get_rule ();
+            r = &fact->get_rule ();
             rules.push_back (const_cast<datalog::rule *> (r));
             TRACE ("spacer",
                     tout << "next rule: " << r->name ().str () << "\n";
@@ -2225,7 +2224,7 @@ namespace spacer {
 
         // get and discard query rule
         reach_fact = m_query->get_last_reach_fact ();
-        r = reach_fact->get_rule ();
+        r = &reach_fact->get_rule ();
 
         // initialize queues
         reach_facts.append (reach_fact->get_justifications ());
@@ -2251,7 +2250,7 @@ namespace spacer {
             ptr_vector<pred_transformer> child_pts;
 
             // get justifying rule and child facts for the derivation of reach_fact at pt
-            r = reach_fact->get_rule ();
+            r = &reach_fact->get_rule ();
             const reach_fact_ref_vector &child_reach_facts = 
               reach_fact->get_justifications ();
             // get child pts
@@ -2761,8 +2760,7 @@ namespace spacer {
 
         m_stats.m_num_reach_queries++;
         ptr_vector<app> empty;
-        reach_fact *f = alloc(reach_fact, m, res, elim_aux ? empty : aux_vars);
-        f->set_rule (r);
+        reach_fact *f = alloc(reach_fact, m, r, res, elim_aux ? empty : aux_vars);
         for (unsigned i = 0, sz = child_reach_facts.size (); i < sz; ++i)
           f->add_justification (*child_reach_facts [i]);
         return f;
