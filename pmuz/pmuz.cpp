a21670f1214ef852ac080938175a8bc6e0e183de
Sagar Chaki <chaki@sei.cmu.edu>
2015-01-30 17:18:22 -0500

    better creation of parameters.

--- pmuz/pmuz.cpp
+++ pmuz/pmuz.cpp
@@ -6,30 +6,17 @@ extern "C" {
 int main(int argc, char *argv[])
 {
   std::cout << "hello world ...\n";
+
+  //-- set parameters
+  Z3_global_param_set("verbose", "1");
+  Z3_global_param_set("fixedpoint.engine", "spacer");
+
+  //-- create context
   Z3_config config = Z3_mk_config ();
   Z3_context ctx = Z3_mk_context (config);
   Z3_del_config (config);
   Z3_fixedpoint fxpt = Z3_mk_fixedpoint (ctx);
   Z3_fixedpoint_inc_ref (ctx, fxpt);
-
-  //-- get the fixedpoint parameter names from the description.
-  Z3_param_descrs pardesc = Z3_fixedpoint_get_param_descrs(ctx,fxpt);
-  Z3_string descStr = Z3_param_descrs_to_string(ctx,pardesc);
-  //std::cout << descStr << '\n';
-
-  //-- create fixedpoint parameters
-  Z3_params params = Z3_mk_params(ctx);
-  Z3_params_inc_ref(ctx,params);
-  Z3_params_set_symbol(ctx,params, Z3_mk_string_symbol(ctx, "engine"), Z3_mk_string_symbol(ctx, "spacer"));
-  //Z3_params_set_bool(ctx,params, Z3_mk_string_symbol(ctx,"bit_blast"), false);
-
-  //-- validate parameters
-  pardesc = Z3_fixedpoint_get_param_descrs(ctx,fxpt);
-  Z3_params_validate(ctx,params,pardesc);
-  
-  //-- set fixedpoint parameters and free parameters
-  Z3_fixedpoint_set_params(ctx,fxpt,params);
-  Z3_params_dec_ref(ctx,params);
 
   //-- load the problem from file
   Z3_ast_vector queries = Z3_fixedpoint_from_file (ctx, fxpt, argv[1]);
