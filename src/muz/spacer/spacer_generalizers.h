667bf95d1f338a19709d443ea1e89eab0909fb37
Arie Gurfinkel <arie@cmu.edu>
2016-04-26 15:26:18 -0400

    removed legacy generalizers
    These might be used in Gpdr, but we have never used them in Spacer.

--- src/muz/spacer/spacer_generalizers.h
+++ src/muz/spacer/spacer_generalizers.h
@@ -51,49 +51,6 @@ namespace spacer {
     class r_map : public map<rational, T, rational::hash_proc, rational::eq_proc> {
     };
 
-    class core_arith_inductive_generalizer : public core_generalizer {
-        typedef std::pair<expr*, unsigned> term_loc_t;
-        typedef r_map<vector<term_loc_t> > bounds_t;
-
-        ast_manager&    m;
-        arith_util      a;
-        expr_ref_vector m_refs;
-        bounds_t        m_lb;
-        bounds_t        m_ub;
-
-        struct eq {
-            expr*    m_term;
-            rational m_value;
-            unsigned m_i;
-            unsigned m_j;
-            eq(expr* t, rational const& r, unsigned i, unsigned j): m_term(t), m_value(r), m_i(i), m_j(j) {}
-        };
-        void reset();
-        void insert_bound(bool is_lower, expr* x, rational const& r, unsigned i);
-        void get_eqs(expr_ref_vector const& core, svector<eq>& eqs);
-        bool substitute_alias(rational const&r, expr* x, expr* e, expr_ref& result);
-    public:
-        core_arith_inductive_generalizer(context& ctx);
-        virtual ~core_arith_inductive_generalizer() {}
-        virtual void operator()(model_node& n, expr_ref_vector& core, unsigned& uses_level);
-    };
-
-    class core_multi_generalizer : public core_generalizer {
-        core_bool_inductive_generalizer m_gen;
-    public:
-        core_multi_generalizer(context& ctx, unsigned max_failures): core_generalizer(ctx), m_gen(ctx, max_failures) {}
-        virtual ~core_multi_generalizer() {}
-        virtual void operator()(model_node& n, expr_ref_vector& core, unsigned& uses_level);
-        virtual void operator()(model_node& n, expr_ref_vector const& core, unsigned uses_level, cores& new_cores);
-    };
-
-    class core_induction_generalizer : public core_generalizer {
-        class imp;
-    public:
-        core_induction_generalizer(context& ctx): core_generalizer(ctx) {}
-        virtual ~core_induction_generalizer() {}
-        virtual void operator()(model_node& n, expr_ref_vector& core, unsigned& uses_level);
-    };
   
   class core_array_eq_generalizer : public core_generalizer 
   {
