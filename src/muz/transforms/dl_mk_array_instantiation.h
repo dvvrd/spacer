e7c12dab1691a02427b1923c091f9dd02417a7d4
jbraine <julien.braine@ens-lyon.fr>
2016-11-01 11:31:21 -0700

    Made documentation of instantiation clearer

--- src/muz/transforms/dl_mk_array_instantiation.h
+++ src/muz/transforms/dl_mk_array_instantiation.h
@@ -9,15 +9,14 @@ Abstract:
     
     Motivation   :  Given a predicate P(a), no quantifier-free solution can express that P(a) <=> forall i, P(a[i]) = 0
 
-    Solution     :  We introduce a fresh variable i, and transform P(a) into P#(i, a). Thus, discovering that P#(i,a) <=> a[i] = 0, 
-                    is equivalent to discovering forall i, P(a[i]) = 0.
-                    The impact on Horn rules is the following :
-                    Let us take a Horn rule : P(a, args) /\ phi(a, args, args') => P'(args') (for simplicity, assume there are no arrays in args').
-                    We do not only transform it into P#(i, a, args) /\ phi(a, args, args') => P'(args'), doing so would allow phi only to read 
-                    from one chosen cell i. Instead, we transform it into  (/\_r in read_indices(phi) P#(r, a, args)) /\ phi(a, args, args') => P'(args').
+    Solution     :  Introduce a fresh variable i, and transform P(a) into P!inst(i, a). 
+                    Now, (P!inst(i,a) := a[i] = 0) <=> P(a) := forall i, a[i] = 0.
+
+                    Transformation on Horn rules:
+                    P(a, args) /\ phi(a, args, args') => P'(args') (for simplicity, assume there are no arrays in args').
+                    Is transformed into:
+                    (/\_r in read_indices(phi) P!inst(r, a, args)) /\ phi(a, args, args') => P'(args')
                     
-
-
     Limitations  :  This technique can only discover invariants on arrays that depend on one quantifier.
     Related work :  Techniques relying on adding quantifiers and eliminating them. See dl_mk_quantifier_abstraction and dl_mk_quantifier_instantiation
 
@@ -34,14 +33,23 @@ Implementation:
        corresponds to fixedpoint.xform.instantiate_arrays.enforce
 
     3) Adding slices in the mix -> We wish to have the possibility to further restrict the search space: we want to smash cells, given a smashing rule.
-       For example, in for loops j=0; j<n; j++, it might be relevant to restrict the search space and look for invariants that only depend on whether i,
-       the quantified variable, is between [0,j[ or ]j, +oo[.
-       This means we are given a function GetId : Index -> SetIds, that given an index, returns its id. In our example, GetId(i) could be
-       ite(i<j, true, false).
+       For example, in for loops j=0; j<n; j++, it might be relevant to restrict the search space and look for invariants that only depend on whether 
+       0<=i<j or j<=i, where i is the quantified variable.
+
+       Formally, a smashing rule is a function from the Index set (usually integer) to integers (the id set). 
+       GetId(i) should return the id of the set i belongs in.
+       
+       In our example, we can give 0 as the id of the set {n, 0<=n<j} and 1 for the set {n, j<=n}, and -1 for the set {n, n<0}. We then have
+       GetId(i) = ite(i<0, -1, ite(i<j, 0, 1))
+
        Given that GetId function, P(a) /\ phi(a, ...) => P'(...) is transformed into 
        (/\_r in read_indices(phi) P(id_r, a[r], a) /\ GetId(r) = id_r) /\ phi(a, ...) => P'(...).
        Note : when no slicing is done, GetId(i) = i.
        This option corresponds to fixedpoint.xform.instantiate_arrays.slice_technique
+
+       Although we described GetId as returning integers, there is no reason to restrict the type of ids to integers. A more direct method, 
+       for the 0<=i<j or j<=i case could be :
+       GetId(i) = (i<0, i<j)
 
     4) Reducing the set of r in read_indices(phi): in fact, we do not need to "instantiate" on all read indices of phi, 
        we can restrict ourselves to those "linked" to a, through equalities and stores.
